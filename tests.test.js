edb25b4404951e45e573eac5844683050158ae9f
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-08-13 13:52:41 +1200

    install cheerio as dev dependency. implement some tests of the routes

--- tests.test.js
+++ tests.test.js
@@ -1,3 +1,33 @@
+const request = require('supertest')
+const cheerio = require('cheerio')
+const server = require('./server')
+
 test('working', () => {
   expect(1 + 2).toBe(3)
+})
+
+test('GET / returns home page with list of places', (done) => {
+  request(server)
+    .get('/')
+    .expect(200)
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+      const placeElems = $('img')
+      expect(placeElems).not.toBeNull()
+      expect(err).toBeNull()
+      done()
+    })
+})
+
+test('GET /places/:id returns details of a specific place', (done) => {
+  request(server)
+    .get('/places/1')
+    .expect(200)
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+      const placeName = $('h2').text()
+      expect(placeName).toMatch('Milford')
+      expect(err).toBeNull()
+      done()
+    })
 })
